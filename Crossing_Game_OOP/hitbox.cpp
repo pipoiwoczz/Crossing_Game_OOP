d0f161c516f035e993c233785262145d0e662b7c
Hoàng Túy Minh <tuyminh.hoang@gmail.com>
2023-11-01 22:34:12 +0700

    moving from other branch

--- Crossing_Game_OOP/hitbox.cpp
+++ Crossing_Game_OOP/hitbox.cpp
@@ -2,21 +2,23 @@
 
 bool Hitbox::isOverlap(Hitbox h)
 {
-    if (h.topleft.X<= botright.X && h.botright.X >= topleft.X
+    if (h.topleft.X <= botright.X && h.botright.X >= topleft.X
         && h.topleft.Y <= botright.Y && h.botright.Y >= topleft.Y)
         return true;
     else
         return false;
 }
 
-void Hitbox::getHitbox(COORD& topleftGet, COORD& botrightGet)
+void Hitbox::getHitbox(COORD &topleftGet, COORD &botrightGet)
 {
     topleftGet = topleft;
     botrightGet = botright;
 }
 
-void Hitbox::move(const int move_x, const int move_y)
+void Hitbox::move(const COORD displace)
 {
-    topleft = { short(topleft.X + move_x), short(topleft.Y + move_y) };
-    botright = { short(botright.X + move_x), short(botright.Y + move_y) };
+    topleft.X += displace.X;
+    topleft.Y += displace.Y;
+    botright.X += displace.X;
+    botright.Y += displace.Y;
 }
--- Crossing_Game_OOP/hitbox.h
+++ Crossing_Game_OOP/hitbox.h
@@ -2,6 +2,8 @@
 #define hitbox_h
 
 #include "setup.h"
+#include <Windows.h>
+
 class cObstacle;
 class cPeople;
 
@@ -10,11 +12,11 @@ class Hitbox
 private:
     COORD topleft;
     COORD botright;
-
+    
 public:
-    void getHitbox(COORD& topleftGet, COORD& botrightGet);
-    void move(const int move_x, const int move_y);
-    bool isOverlap(Hitbox h);
+    void getHitbox (COORD &topleftGet, COORD &botrightGet);
+    void move(const COORD displace);
+    bool isOverlap (Hitbox h);
 };
 
 #endif /* hitbox_hpp */
